1af89491a4b12655d4845eccbf86852f200b4925
Yuta Hiroto <git@hiroppy.me>
2024-01-13 09:36:25 +0900

    wip

--- src/app/examples/(route-handlers)/route-handlers-revalidating/revalidate-tag/api/route.ts
+++ src/app/examples/(route-handlers)/route-handlers-revalidating/revalidate-tag/api/route.ts
@@ -4,7 +4,6 @@ import { NextRequest } from "next/server";
 export async function GET(request: NextRequest) {
   const tag = request.nextUrl.searchParams.get("tag") ?? "";
 
-  console.log(tag);
   revalidateTag(tag);
 
   return Response.json({
--- src/app/examples/(route-handlers)/route-handlers-revalidating/revalidate-tag/page.tsx
+++ src/app/examples/(route-handlers)/route-handlers-revalidating/revalidate-tag/page.tsx
@@ -7,6 +7,14 @@ export default function Page() {
   const path = "/examples/route-handlers-revalidating/revalidate-tag/api";
   const tags = useMemo(() => ["dog"], []);
   const { data } = useFetch<{ now: number }>(path, tags);
+  const { data: a } = useFetch<{ now: number }>(path, tags);
+  const { data: b } = useFetch<{ now: number }>(path);
 
-  return <p>revalidated: {data?.now}</p>;
+  return (
+    <div>
+      <p>revalidated: {data?.now}</p>
+      <p>revalidated: {a?.now}</p>
+      <p>revalidated: {b?.now}</p>
+    </div>
+  );
 }
