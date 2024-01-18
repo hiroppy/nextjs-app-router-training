c6832a1fbeb772fa544eb011498a4da24234ca46
Yuta Hiroto <git@hiroppy.me>
2024-01-18 23:06:06 +0900

    fix: remove performance.now

--- src/app/api/now/route.ts
+++ src/app/api/now/route.ts
@@ -1,10 +1,9 @@
-import { performance } from "node:perf_hooks";
 import { NextResponse } from "next/server";
 
 export const revalidate = 0;
 
 export async function GET() {
   return NextResponse.json({
-    now: performance.now(),
+    now: Date.now(),
   });
 }
