6c31abeac356c62de4842da26e37f9e2e209b26c
Yuta Hiroto <git@hiroppy.me>
2024-01-20 23:49:52 +0900

    fix: use unstable_noStore

--- src/app/examples/(caching)/data-cache/no-store/page.tsx
+++ src/app/examples/(caching)/data-cache/no-store/page.tsx
@@ -1,11 +1,12 @@
 import { Boundary } from "@/app/_components/boundary";
 import { NOW_API_ENDPOINT, NowResponse } from "@/constants";
+import { unstable_noStore } from "next/cache";
 
 export default async function Page() {
+  unstable_noStore();
+
   const { now: noStore }: NowResponse = await (
-    await fetch(NOW_API_ENDPOINT, {
-      cache: "no-store",
-    })
+    await fetch(NOW_API_ENDPOINT)
   ).json();
 
   return <Boundary label="No Store">{noStore}</Boundary>;
